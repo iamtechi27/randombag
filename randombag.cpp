2800ba4054bd3f373f7018e660d859cfa1736486
iamtechi27 <iamtechi27@gmail.com>
2015-10-13 17:01:21 -0700

    Added comments should also be noted that I added a space every 7 characters in the previous commit. Makes it easier to read and check for errors.

--- randombag.cpp
+++ randombag.cpp
@@ -9,27 +9,27 @@ string bag;
 
 void init() {
 	bag = "OISZLJT";
-}
+} //init()
 
 char draw() {
 	int randomNumber;
 	char result;
 	if (bag.length() == 0) {
 		init();
-	}
+	} //if (bag.length() == 0)
 	randomNumber = rand() % bag.length();
 	result = bag.at(randomNumber);
 	bag.erase(bag.begin()+randomNumber);
 	return result;
-}
+} //draw()
 
 int main() {
 	srand(time(NULL));
 	for (int i = 0; i < 50; i++) {
 		if (i % 7 == 0) {
 			cout << " ";
-		}
+		} //if (i % 7 == 0)
 		printf("%c", draw());
-	}
+	} //for (int i < 50)
 	cout << endl;
 }
